1a6333e421dc4b67f1862e1d91e0423b6d98626c
Abe Schor <schor.abe@gmail.com>
2019-01-31 12:35:50 -0500

    iframe getting deleted

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false, // disable same-origin-policy in the shell window, webviews have it restored
+      allowRunningInsecureContent: false,
+      //nativeWindowOpen: true,
+      nodeIntegration: true,
+      webviewTag: true,
+      nodeIntegrationInSubFrames: true,
+      preload: path.join(__dirname, 'renderer.js')
+    },
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -27,7 +39,13 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+
+  ipcMain.on('preload-ran', (event, arg) => {
+    console.log('preload-ran')
+  })
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { ipcRenderer, webFrame } = require('electron')
+
+ipcRenderer.send('preload-ran', window.location.href, webFrame.routingId)
+
+ipcRenderer.on('preload-ping', () => {
+  ipcRenderer.send('preload-pong', webFrame.routingId)
+})
